b7e8a6099e226e777ebab42942733a487bdae20d
Vicente <vicente@rankia.com>
2014-11-20 11:51:34 +0100

    Changed dir organization to access styleguide on 'http://domain.com/styleguide/'.

--- lib/psique/engine.rb
+++ lib/psique/engine.rb
@@ -2,10 +2,15 @@ module Psique
   module Rails
     class Engine < ::Rails::Engine
       isolate_namespace Psique::Rails
+
       initializer 'psique.assets.precompile' do |app|
         %w(stylesheets javascripts fonts images).each do |sub|
           app.config.assets.paths << root.join('assets', sub).to_s
         end
+      end
+
+      initializer 'static assets' do |app|
+        app.middleware.use ::ActionDispatch::Static, "#{root}/doc/public"
       end
     end
   end
