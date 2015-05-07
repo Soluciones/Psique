f38261a3741228c0b6e1d52fa19764c5061c920c
Francisco Javier Alapont Casero <caserofrancisco@gmail.com>
2015-05-07 17:42:48 +0200

    Added color pages for Rankia, Verema, and styleguide

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Psique::Engine.routes.draw do
   root to: 'documentation#index'
-  get '/elements', to:'documentation#elements'
+  get '/colours/verema', to:'documentation#verema_colors'
+  get '/colours/rankia', to:'documentation#rankia_colors'
+  get '/colours/guide', to:'documentation#guide_colors'
 end
