0c1dd7319b327ea6247c4e1e72cdaebe132069f7
Francisco Javier Alapont Casero <caserofrancisco@gmail.com>
2015-05-08 14:03:55 +0200

    Added new typography and icon fonts to styleguide

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Psique::Engine.routes.draw do
   root to: 'documentation#index'
-  get '/colours/verema', to:'documentation#verema_colors'
-  get '/colours/rankia', to:'documentation#rankia_colors'
-  get '/colours/guide', to:'documentation#guide_colors'
+  get '/ui-elements/colours/verema', to:'documentation#verema_colors'
+  get '/ui-elements/colours/rankia', to:'documentation#rankia_colors'
+  get '/ui-elements/colours/guide', to:'documentation#guide_colors'
+  get '/ui-elements/fonts', to:'documentation#font'
+  get '/ui-elements/icon-font', to:'documentation#font_awesome'
 end
