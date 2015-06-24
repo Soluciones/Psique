6e26e433d19ab106107a04e3541016d189ba4f96
Francisco Javier Alapont Casero <Fran@iMac-de-Francisco.local>
2015-06-24 17:43:36 +0200

    Changed variable names and some blocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Psique::Engine.routes.draw do
   root to: 'documentation#index'
-  get '/ui-elements/colours/verema', to:'documentation#verema_colors'
-  get '/ui-elements/colours/rankia', to:'documentation#rankia_colors'
-  get '/ui-elements/colours/guide', to:'documentation#guide_colors'
+  get '/ui-elements/colors/guide', to:'documentation#guide_colors'
   get '/ui-elements/fonts', to:'documentation#font'
   get '/ui-elements/icon-font', to:'documentation#font_awesome'
-  get '/ui-elements/ui-blocks', to:'documentation#ui_blocks'
+  get '/ui-elements/blocks', to:'documentation#ui_blocks'
 end
